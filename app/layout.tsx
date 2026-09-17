271bff847b9180a05370a6f31abfac2bd5d71767
lastro1206 <irene051206@naver.com>
2026-09-17 16:29:25 +0900

    feat: 푸터 컴포넌트 구현 (#31)

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { pretendard } from "@/app/fonts";
 import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
 import ThemeProvider from "@/components/layout/ThemeProvider";
 
 export const metadata: Metadata = {
@@ -51,6 +52,7 @@ export default function RootLayout({
         >
           <Header />
           <main className="flex-1 flex flex-col">{children}</main>
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
